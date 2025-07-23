77a048059974e419d19350694358e29178fd67d0
padakantidilip <22eg107c22@anurag.edu.in>
2025-07-23 11:56:12 +0530

    Epoch vs Accuracy on train and validation data for 100 epochs

--- cifar_mlp.py
+++ cifar_mlp.py
@@ -22,7 +22,7 @@ model.add(Dense(units=10,activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 #train
-history = model.fit(x_train,y_train,epochs=30,batch_size=64,validation_split=0.2)
+history = model.fit(x_train,y_train,epochs=100,batch_size=64,validation_data=(x_test,y_test))
 print(history.history.items())
 print(history.history.keys())
 #evaluate
